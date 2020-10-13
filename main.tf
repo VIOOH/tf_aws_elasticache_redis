7ae86f55d3251ab60a2cbaf97b7f155e4dc9145f
Andrew Stangl <andrewstangl@gmail.com>
2020-10-13 14:33:27 +0200

    TF deprecation warning fixes

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_elasticache_replication_group" "redis" {
   automatic_failover_enabled    = var.redis_failover
   engine_version                = var.redis_version
   port                          = var.redis_port
-  parameter_group_name          = "${var.redis_clusters == null ? aws_elasticache_parameter_group.redis_parameter_group_cluster[0].id : aws_elasticache_parameter_group.redis_parameter_group[0].id}"
+  parameter_group_name          = var.redis_clusters == null ? aws_elasticache_parameter_group.redis_parameter_group_cluster[0].id : aws_elasticache_parameter_group.redis_parameter_group[0].id
   subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.id
   security_group_ids            = [aws_security_group.redis_security_group.id]
   apply_immediately             = var.apply_immediately
