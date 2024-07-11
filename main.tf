12bdd7569d3dca98eccb0fc4102c8db2ea8b232d
Jignesh Shah <36189390+rspljignesh@users.noreply.github.com>
2024-07-11 16:38:33 +0530

    Added support for parameter group for redis version 7

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_elasticache_parameter_group" "redis_parameter_group" {
   description = "Terraform-managed ElastiCache parameter group for ${var.name}-${var.env}"
 
   # Strip the patch version from redis_version var
-  family = "redis${replace(var.redis_version, "/\\.[\\d]+$/", "")}"
+  family = substr(var.redis_version, 0,1) < 6 ?  "redis${replace(var.redis_version, "/\\.[\\d]+$/", "")}": "redis${substr(var.redis_version, 0,1)}"
 
 
   dynamic "parameter" {
